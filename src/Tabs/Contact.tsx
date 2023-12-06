82586a0b3a6388e232979ab061fbcf910a240dd9
MirzaUsman733 <125288010+MirzaUsman733@users.noreply.github.com>
2023-12-06 15:05:46 +0500

    add the Contact page in reactNative

--- src/Tabs/Contact.tsx
+++ src/Tabs/Contact.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import {ScrollView} from 'react-native';
 import {
   View,
   Text,
@@ -7,72 +7,71 @@ import {
   TouchableOpacity,
   StyleSheet,
 } from 'react-native';
-import { useTheme } from '../Components/ThemeContext';
+import {useTheme} from '../Components/ThemeContext';
 
 const Contact: React.FC = () => {
-    const {backgroundColor, textColor} = useTheme();
+  const {backgroundColor, textColor} = useTheme();
 
-    return (
-      <ScrollView contentContainerStyle={styles.scrollContainer}>
-        <View style={[styles.container, {backgroundColor}]}>
-          {/* Contact Info */}
-          <View style={styles.contactInfo}>
-            <Text style={styles.header}>Contact Info</Text>
-            <View style={styles.info}>
-              <View style={styles.infoItem}>
-                <Text style={{color: textColor}}>
-                  184 Ippokratous Street, Athens, Gr, 11472
-                </Text>
-              </View>
-              <View style={styles.infoItem}>
-                <Text style={{color: textColor}}>nassosanagn@gmail.com</Text>
-              </View>
-              <View style={styles.infoItem}>
-                <Text style={{color: textColor}}>702-279-3488</Text>
-              </View>
+  return (
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={[styles.container, {backgroundColor}]}>
+        <View style={styles.contactInfo}>
+          <Text style={styles.header}>Contact Info</Text>
+          <View style={styles.info}>
+            <View style={styles.infoItem}>
+              <Text style={{color: textColor}}>
+                184 Ippokratous Street, Athens, Gr, 11472
+              </Text>
             </View>
-          </View>
-
-          {/* Contact Form */}
-          <View style={styles.contactForm}>
-            <Text style={styles.header}>Send a Message</Text>
-            <View style={styles.formBox}>
-              <TextInput
-                style={styles.input}
-                placeholder="First Name"
-                placeholderTextColor="rgba(0, 0, 0, 0.5);"
-              />
-              <TextInput
-                style={styles.input}
-                placeholder="Last Name"
-                placeholderTextColor="rgba(0, 0, 0, 0.5);"
-              />
-              <TextInput
-                style={styles.input}
-                placeholderTextColor="rgba(0, 0, 0, 0.5);"
-                placeholder="Email Address"
-                keyboardType="email-address"
-              />
-              <TextInput
-                style={styles.input}
-                placeholderTextColor="rgba(0, 0, 0, 0.5);"
-                placeholder="Mobile Number"
-                keyboardType="phone-pad"
-              />
-              <TextInput
-                style={[styles.input, styles.textarea]}
-                multiline={true}
-                placeholderTextColor="rgba(0, 0, 0, 0.5);"
-                placeholder="Write your message here..."
-              />
-              <TouchableOpacity style={styles.submitButton}>
-                <Text style={{color: '#fff'}}>Send</Text>
-              </TouchableOpacity>
+            <View style={styles.infoItem}>
+              <Text style={{color: textColor}}>nassosanagn@gmail.com</Text>
+            </View>
+            <View style={styles.infoItem}>
+              <Text style={{color: textColor}}>702-279-3488</Text>
             </View>
           </View>
         </View>
-      </ScrollView>
-    );
+
+        {/* Contact Form */}
+        <View style={styles.contactForm}>
+          <Text style={styles.header}>Send a Message</Text>
+          <View style={styles.formBox}>
+            <TextInput
+              style={styles.input}
+              placeholder="First Name"
+              placeholderTextColor="rgba(0, 0, 0, 0.5);"
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Last Name"
+              placeholderTextColor="rgba(0, 0, 0, 0.5);"
+            />
+            <TextInput
+              style={styles.input}
+              placeholderTextColor="rgba(0, 0, 0, 0.5);"
+              placeholder="Email Address"
+              keyboardType="email-address"
+            />
+            <TextInput
+              style={styles.input}
+              placeholderTextColor="rgba(0, 0, 0, 0.5);"
+              placeholder="Mobile Number"
+              keyboardType="phone-pad"
+            />
+            <TextInput
+              style={[styles.input, styles.textarea]}
+              multiline={true}
+              placeholderTextColor="rgba(0, 0, 0, 0.5);"
+              placeholder="Write your message here..."
+            />
+            <TouchableOpacity style={styles.submitButton}>
+              <Text style={{color: '#fff'}}>Send</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
